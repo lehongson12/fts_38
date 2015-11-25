ed173d106268794c6a8275041e82432af69c0b74
lehongson12 <lehongson.bkvn@gmail.com>
2015-11-25 14:11:52 +0700

    admin create category

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,56 @@
+class Admin::CategoriesController < ApplicationController
+  before_action :admin_only
+
+  def index
+    @categories = Category.paginate page: params[:page], per_page: Settings.length.page
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit
+    @category = Category.find params[:id]
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = t "category.create.success"
+      redirect_to [:admin, :categories]
+    else
+      flash[:danger] = t "category.create.error"
+      render :new
+    end
+  end
+
+  def update
+    @category = Category.find params[:id]
+    if @category.update_attributes category_params
+      flash[:success] = t "category.update.success"
+      redirect_to [:admin, :categories]
+    else
+      flash[:danger] = t "category.update.error"
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    if @category.destroy
+      flash[:success] = t "category.destroy.success"
+      redirect_to [:admin, :categories]
+    else
+      flash[:danger] = t "category.destroy.error"
+    end
+  end
+
+  private
+  def admin_only
+    redirect_to root_path unless current_user.try :admin?  
+  end
+
+  def category_params
+    params.require(:category).permit :name, :description
+  end
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :exams
   has_many :questions
+
+  validates :name, presence: true, length: {maximum: 50}, uniqueness: true
+  validates :description, presence: true, length: {maximum: 50}
 end
