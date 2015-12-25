f7ee84ac7c82640d6214bafb2bce97c558d55477
lehongson12 <lehongson.bkvn@gmail.com>
2015-12-25 15:13:44 +0700

    rspec test question model with mock stub

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe Question, type: :model do
   it {should belong_to :user}
   it {should belong_to :category}
   it {should accept_nested_attributes_for :answers}
+
+  context "scope" do
+    it "accepted" do
+      question = double("Question")
+      allow(question).to receive(:state) {0}
+      expect(question.state).to eq 0
+    end
+
+    it "random_questions" do
+      question = double("Question")
+      allow(question).to receive(:random_questions) {Settings.exam.limit_questions}
+      expect(question.random_questions).to eq Settings.exam.limit_questions
+    end
+  end
 end
